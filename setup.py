3117704dd3beee4b42af78f5aa1653de5f9eb0cb
Maxime Cabanal <=>
2019-04-11 10:16:40 +0300

    Adding license file + desc fields in setup

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='plouf',
     version='0.1',
+    author="Mazrog",
+    author_email="maxime.cabanal@outlook.fr",
+    description="Little CLI tools to manage projects.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mazrog/plouf",
+    
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
